b02a55e0e4092a2b2fc980ee5f1e57d2837e531c
zkx <632591@qq.com>
2023-07-31 00:15:51 +0800

    启动类上传

---
+++ src/main/java/com/zkx/Spring6HttpInterface.java
@@ -0,0 +1,11 @@
+package com.zkx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring6HttpInterface {
+    public static void main(String[] args) {
+        SpringApplication.run(Spring6HttpInterface.class);
+    }
+}
